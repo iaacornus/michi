4f0ddd9a1ec7566ac2b636b70cfd20af48e93450
yaacornus <96870156+yaacornus@users.noreply.github.com>
2021-12-31 18:41:40 +0800

    upload the parameters for message review

---
+++ src/params.py
@@ -0,0 +1,17 @@
+class scam:
+    scams = ["the crypto giveaway! congratulations! You have been selected a random user in our big giveaways!",
+             "the cryptobtc giveaway! congratulations!","You have been selected a random user in our big giveaways!",
+             "crypto has decided to host a big giveaway cryptocurrencies", "the giveaway", "you become a random winner",
+             "you become a random winner in our giveaway", "to help in this, our company has decided to host a major raffle of funds",
+             "you won btc", "you won crypto", "free gift discord nitro", "free nitro", "free nitro giveaway", "free discord nitro for 1 month",
+             "free nitro", "I have left", "giveaway", "crypto giveaway", "free crypto", "free btc", "i offer a free adult membership",
+             "dm me for the link", "I have a free offer dm me for the link", "I have a free nitro dm me", "I have a free offer, dm me"]
+    
+    scam_link = ["https://discordsgift.com/", "https://freenitrogiveaway.com/", "https://discordnitro.com/", "https://.xyz", "http://.xyz",
+                 "http://.zy"]
+    
+        
+class interesting:
+    interesting = ["physics", "science", "chemistry", "multiverse", "time", "travel", "wormholes", "biology", "do you know",
+               "know", "what", "interesting", "iaacornus", "cell", "culture", "virus", "molecule", "chem", "bio", "thermodynamics",
+               "blachhole"]
