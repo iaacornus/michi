37742c0f263ad5729da4e229847d6015c6337d29
yaacornus <96870156+yaacornus@users.noreply.github.com>
2022-01-01 03:33:25 +0800

    add more references

--- src/params.py
+++ src/params.py
@@ -1,14 +1,15 @@
 class params:
     scams = ["the crypto giveaway! congratulations! You have been selected a random user in our big giveaways!",
              "the cryptobtc giveaway! congratulations!","You have been selected a random user in our big giveaways!",
-             "crypto has decided to host a big giveaway cryptocurrencies", "the giveaway", "you become a random winner",
-             "you become a random winner in our giveaway", "to help in this, our company has decided to host a major raffle of funds",
-             "you won btc", "you won crypto", "free gift discord nitro", "free nitro", "free nitro giveaway", "free discord nitro for 1 month",
-             "free nitro", "I have left", "giveaway", "crypto giveaway", "free crypto", "free btc", "i offer a free adult membership",
-             "dm me for the link", "I have a free offer dm me for the link", "I have a free nitro dm me", "I have a free offer, dm me",
-             "i have a free nitro link", "free nitro", "free link", "i have a free discord nitro", "i have a free discord link",
-             "free discord nitro link", "https://discordsgift.com/", "https://freenitrogiveaway.com/", "https://discordnitro.com/", "https://.xyz", "http://.xyz",
-                 "http://.zy"]
+             "crypto has decided to host a big giveaway cryptocurrencies", "you become a random winner", "you become a random winner in our giveaway",
+             "to help in this, our company has decided to host a major raffle of funds", "you won btc", "you won crypto",
+             "free gift discord nitro", "free nitro", "free nitro giveaway", "free discord nitro for 1 month", "you won free nitro",
+             "free nitro", "crypto giveaway", "free crypto", "free btc", "i offer a free adult membership", "dm me for the link",
+             "I have a free offer dm me for the link", "I have a free nitro dm me", "I have a free offer, dm me",
+             "i have a free nitro link", "i have a free discord nitro", "i have a free discord link", "free discord nitro link",
+             "https://discordsgift.com/", "https://freenitrogiveaway.com/", "https://discordnitro.com/", "https://.xyz", "http://.xyz",
+             "http://.zy", "free discord nitro", "i am giving away free nitro", "free nitro giveaway", "random user winner",
+             "the crypto giveaway!"]
     
     thank_message = ["thanks", "thank you", "thx", "thank", "thank you very much", "arigato", "arigatogozaimasu",
                      "gozaimasu", "arigatou", "arigatogozaimas", "mercie", "gracias", "gamsa", "gamsa hayeo"]
