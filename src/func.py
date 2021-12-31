9c8ec4bd770a45d1ba440f927356da809b4773b9
yaacornus <96870156+yaacornus@users.noreply.github.com>
2021-12-31 14:14:58 +0800

    return url and improved suggestion for wrong word

--- src/func.py
+++ src/func.py
@@ -1,48 +1,35 @@
 import wikipediaapi
-import re
-import requests
-from bs4 import BeautifulSoup as bs
 import urllib.request
-from difflib import SequenceMatcher
+import enchant
 
 def get_defin(define):
+    wiki_wiki = wikipediaapi.Wikipedia('en')
+    page_py = wiki_wiki.page(define)
 
-  wiki_wiki = wikipediaapi.Wikipedia('en')
-  page_py = wiki_wiki.page(define)
+    alpha = list("abcdefghijklmnopqrstuvwxyz")
+    ALPHA = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-  alpha = list("abcdefghijklmnopqrstuvwxyz")
-  ALPHA = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    comb = []
+    for x in ALPHA:
+        for y in alpha:
+            comb.append(x+y)
 
-
-  comb = []
-  for x in ALPHA:
-    for y in alpha:
-      comb.append(x+y)
-
-  if page_py.exists() is True:
-    defi = f"{page_py.summary}\n\nFull URL = {page_py.fullurl}\nCanonical URL = {page_py.canonicalurl}"
-    
-  else:
-    if (define[0].upper()+define[1].lower()) not in comb:
-      pass
+    if page_py.exists() is True:
+        defi = f"{str(page_py.summary)}"
+        url = f"Full URL = {page_py.fullurl}\nCanonical URL = {page_py.canonicalurl}"
+        
+        return defi, url
     else:
-      wiki = "https://en.wikipedia.org/wiki/Special:AllPages/" + define[0].upper() + define[1].lower()
+        if (define[0].upper()+define[1].lower()) not in comb:
+            return "the given term is not in Wikipedia, kindly correct the term and try again.", 0
+        else:
+            wiki = "https://en.wikipedia.org/wiki/Special:AllPages/" + define[0].upper() + define[1].lower()
+            
+            if urllib.request.urlopen(wiki).getcode() not in [x for x in range(200, 299)]:
+                return "the source is unfortunately down ...", 0
+            else:
+                lang = enchant.Dict("en_US")
+                lang.check(define)
 
-      page = requests.get(wiki)
-
-      if urllib.request.urlopen(wiki).getcode() not in [x for x in range(200, 299)]:
-        pass
-        #await message.channel.send(f"{message.author.mention} the source is unfortunately down ...")
-      else:
-        soup = bs(page.content, "html.parser")
-        topics = soup.find_all("a", class_="mw-redirect")
-
-      dic = {}
-      for x in topics:
-        dic[SequenceMatcher(None, x.get_text(), define).ratio()] = x.get_text()
-      
-      hi = max(dic, key=dic.get)
-      defi = f"Maybe you mean {dic[hi]}?"
-  
-
-  return defi
+                return f"maybe you mean {str(lang.suggest(define)).replace('[', '').replace(']', '')}", 0
+              
