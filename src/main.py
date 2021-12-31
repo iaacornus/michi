ae613d579b901e63941eb53767256faa9b03a4f0
yaacornus <96870156+yaacornus@users.noreply.github.com>
2021-12-31 14:17:39 +0800

    ping user of --define command and return page url

--- src/main.py
+++ src/main.py
@@ -22,67 +22,78 @@ my_id = #<ID>
 
 client = commands.Bot(command_prefix="--")
 
-interesting = ["physics", "science", "chemistry", "multiverse", "time", "travel", "wormholes", "biology", "do you know", "know", "what", "interesting", "iaacornus", "cell", "culture", "virus", "molecule", "chem", "bio", "thermodynamics"]
+interesting = ["physics", "science", "chemistry", "multiverse", "time", "travel", "wormholes", "biology", "do you know",
+               "know", "what", "interesting", "iaacornus", "cell", "culture", "virus", "molecule", "chem", "bio", "thermodynamics"]
 
 print("STARTING BOT")
 
 @client.event
 async def on_ready():
-  print("[BOT ONLINE]")
+    print("[BOT ONLINE]")
 
 @client.event
 async def on_message(message):
   
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  for x in interesting:
-    if x in message.content.lower():
-      await message.channel.send(f"<@{my_id}> there's something interesting...")
-      
-  await client.process_commands(message)
+    for x in interesting:
+        if x in message.content.lower():
+            await message.channel.send(f"<@{my_id}> there's something interesting...")
+            
+    await client.process_commands(message)
 
 @client.command(name="topic")
 async def topic(ctx):
-  sections = ["space", "health", "planet-earth", "strange-news", "animals", "history"]
-  link = "https://www.livescience.com/" + random.choice(sections)
-  page = requests.get(link)
+    sections = ["space", "health", "planet-earth", "strange-news", "animals", "history"]
+    link = "https://www.livescience.com/" + random.choice(sections)
+    page = requests.get(link)
 
-  if urllib.request.urlopen(link).getcode() not in [x for x in range(200, 299)]:
-    await ctx.channel.send(f"<@{message.author.id}> the source is unfortunately down ...")
-  else:
-    soup = bs(page.content, "html.parser")
-    topics = (soup.find_all("h3", class_="article-name"))
+    if urllib.request.urlopen(link).getcode() not in [x for x in range(200, 299)]:
+        await ctx.channel.send(f"<@{message.author.id}> the source is unfortunately down ...")
+    else:
+        soup = bs(page.content, "html.parser")
+        topics = (soup.find_all("h3", class_="article-name"))
 
-  topic = str(random.choice(topics))[25:-5]
-  for x in range(10):
-    if ("top" or str(x) or "top " + str(x) or "best") in str(topic):
-      pass
-    else :
-      dec = topic
+    topic = str(random.choice(topics))[25:-5]
+    for x in range(10):
+        if ("top" or str(x) or "top " + str(x) or "best") in str(topic):
+            pass
+        else :
+            dec = topic
 
-  await ctx.channel.send(f"<@{ctx.author.id}> {dec}")
+    await ctx.channel.send(f"<@{ctx.author.id}> {dec}")
 
 
 @client.command(name="define")
 async def wiki(ctx, word):
-  mess = get_defin(word)
+    mess, url = get_defin(word)
 
-  if len(mess) > 1800:
-    wc = len(mess) ; delimiter = 1800 ; start = 0
-    stop = False
+    if len(mess) > 1800:
+        wc = len(mess) ; delimiter = 1800 ; start = 0
+        stop = False
 
-    while stop is False:
-      
-      if wc > 1800 :
-        await ctx.channel.send(f"<@{ctx.author.id}> {str(mess)[start:delimiter]}-")
-        start += 1800 ; delimiter += 1800 ; wc -= 1800      
-      elif wc < 1800:
-        await ctx.channel.send(f"-{str(mess)[start:delimiter-wc]}")
-        stop = True
+        while stop is False:
+            
+            if wc > 1800 :
+                if start == 0:
+                    await ctx.channel.send(f"<@{ctx.author.id}> {str(mess)[start:delimiter]}-")
+                else:
+                    await ctx.channel.send(f"-{str(mess)[start:delimiter]}-")
+                
+                start += 1800 ; delimiter += 1800 ; wc -= 1800      
+                
+            elif wc < 1800:
+                await ctx.channel.send(f"-{str(mess)[start:delimiter+wc]}")
+                await ctx.channel.send(url)
+                stop = True
 
-  else:
-    await ctx.channel.send(mess)
+    else:
+        if url == 0:
+            await ctx.channel.send(f"<@{ctx.author.id}> {mess}")
+        else:
+            await ctx.channel.send(f"<@{ctx.author.id}> {mess}")
+            await ctx.channel.send(url)
 
 
 
